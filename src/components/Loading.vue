c7b9d0691541987fb984d6d4526f895c824a206f
jkchao <jkchaom@gmail.com>
2018-06-20 14:40:45 +0800

    Loading.vue

---
+++ src/components/Loading.vue
@@ -0,0 +1,47 @@
+<template>
+  <div
+    class="vue-loading"
+    v-html="svg"
+    :style="loadingStyle">
+    
+  </div>
+</template>
+
+<script lang="ts">
+import * as svgLoading from '../svg'
+
+import { Component, Vue, Prop } from 'vue-property-decorator'
+
+@Component({
+  name: 'vueLoading'
+})
+export default class VueLoading extends Vue {
+  @Prop({ default: 'bead' })
+  private type: string
+
+  @Prop({ default: '#5ac1dd'})
+  private color: string
+
+  @Prop({ default: '#5ac1dd'})
+  private size: { width: string, height: string }
+
+  private get svg () {
+    return  (svgLoading as { [key: string ]: any })[this.type]
+  }
+
+  private get loadingStyle () {
+    return {
+      fill: this.color,
+      width: this.size.width,
+      height: this.size.height
+    }
+  }
+}
+
+</script>
+
+<style lang="scss" scoped>
+.vue-loading {
+  margin: auto;
+}
+</style>
