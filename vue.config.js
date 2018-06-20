1a84b5122e0c7f7593598bc9218e91c4ab2fb3e5
jkchao <jkchaom@gmail.com>
2018-06-20 09:48:29 +0800

    change the baseUrl when build

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  assetsDir: './',
+  baseUrl: process.env.NODE_ENV === 'production'
+    ? '/vue-loading/'
+    : '/',
   chainWebpack: config => {
     const svgRule = config.module.rule('svg')
 
